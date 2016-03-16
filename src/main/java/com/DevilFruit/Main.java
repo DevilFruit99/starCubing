f2142e2f2cfd49ea87d921be7457cbd1a3688ffc
DevilFruit99 <sushalpenugonda@gmail.com>
2016-03-16 02:07:28 -0400

    add methods to read from file

--- src/main/java/com/DevilFruit/Main.java
+++ src/main/java/com/DevilFruit/Main.java
@@ -5,18 +5,24 @@ import org.apache.commons.csv.CSVParser;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello World");
-        File csvData = new File("/data/DressSales.csv");
+        //TODO get file path from args value
+        File csvData = new File("C:\\Users\\susha\\IdeaProjects\\starCubing\\data");
 
         try {
-            CSVParser parser = CSVParser.parse("/data/DressSales.csv", CSVFormat.EXCEL);
+
+            CSVParser parser = CSVParser.parse(csvData, Charset.defaultCharset(), CSVFormat.EXCEL);
+            System.out.println(parser.getRecordNumber());
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
     }
 }
